c0a0ce6d7408447c9182abdde8627c5cae7c7de8
tanvighai <tanvighai02@gmail.com>
2023-12-15 20:07:05 -0500

    test commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -80,7 +80,7 @@ resource "aws_route_table" "private" {
   }
   route {
     cidr_block = var.default_vpc_cidr
-    vpc_peering_id = aws_vpc_peering_connection.peering
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
   }
   tags = {
     Name = "private"
