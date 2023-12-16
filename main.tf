89165b16202200490346683eed9de94d2b06c316
tanvighai <tanvighai02@gmail.com>
2023-12-15 20:02:22 -0500

    test commit

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "vpc" {
   azs                    = var.azs
   account_no             = var.account_no
   default_vpc_id         = var.default_vpc_id
-  #default_vpc_cidr       = var.default_vpc_cidr
+  default_vpc_cidr       = var.default_vpc_cidr
 #  default_route_table_id = var.default_route_table_id
 }
 
