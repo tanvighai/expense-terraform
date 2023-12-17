2b22351d5bccf6d0e6716fe6aa33eb2dab695146
tanvighai <tanvighai02@gmail.com>
2023-12-17 15:01:37 -0500

    test commit

--- main.tf
+++ main.tf
@@ -63,6 +63,9 @@ module "backend" {
   vpc_id        = module.vpc.vpc_id
   subnets = module.vpc.private_subnets
   bastion_node_cidr = var.bastion_node_cidr
+  desired_capacity = var.desired_capacity
+  max_size         = var.max_size
+  min_size         = var.min_size
 }
 
 
