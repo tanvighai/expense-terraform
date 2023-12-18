296f8d6b1420b7f74ebe048a162a13992354d298
tanvighai <tanvighai02@gmail.com>
2023-12-18 14:45:14 -0500

    test commit

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#first specify what type of file it is
-
 
 dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
 pip3.11 install boto3 botocore | tee -a /opt/userdata.log
-ansible-pull -localhost , -U https://github.com/tanvighai/infra-ansible.git main.yml -e role_name=${role_name} | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/tanvighai/infra-ansible main.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
