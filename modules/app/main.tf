7cc6f4540a49758503e885f0b86f384b8b77d53e
tanvighai <tanvighai02@gmail.com>
2023-12-18 14:49:06 -0500

    test commit

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -49,6 +49,7 @@ resource "aws_launch_template" "template" {
   vpc_security_group_ids = [aws_security_group.security_group.id]
   user_data = base64encode(templatefile("${path.module}/userdata.sh" ,{
     role_name = var.component
+    env =var.env
 }))
   #templatefile("${path.module}/backends.tftpl", { port = 8080, ip_addrs = ["10.0.0.1", "10.0.0.2"] })
   tag_specifications {
