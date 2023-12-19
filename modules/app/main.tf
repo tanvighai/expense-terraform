8b25a966b930e142f717212991be1befff5d8e85
tanvighai <tanvighai02@gmail.com>
2023-12-19 17:21:41 -0500

    test commit

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -142,6 +142,15 @@ resource "aws_lb_target_group" "tg" {
   port     = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    interval = 5
+    unhealthy_threshold = 2
+    port = var.app_port
+    path = "/health"
+    timeout = 3
+  }
 }
 
 
