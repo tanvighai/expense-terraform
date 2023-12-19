66011481531550b89fd5338a8c63535b5492b1fe
tanvighai <tanvighai02@gmail.com>
2023-12-19 16:51:39 -0500

    test commit

--- main.tf
+++ main.tf
@@ -57,6 +57,7 @@ module "private-lb" {
 #
 ##module for backend server
 module "backend" {
+  depends_on = [module.mysql]
   source = "./modules/app"
   app_port      = 8080
   component     = "backend"
