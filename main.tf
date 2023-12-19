f76975efc4305c142defc1c7db88f3525cf55886
tanvighai <tanvighai02@gmail.com>
2023-12-19 17:29:40 -0500

    test commit

--- main.tf
+++ main.tf
@@ -39,21 +39,21 @@ module "private-lb" {
 #
 ##module for instances
 ##module for frontend server
-#module "frontend" {
-#  source = "./modules/app"
-#  app_port      = 80
-#  component     = "frontend"
-#  env           = var.env
-#  instance_type = "t3.micro"
-#  vpc_cidr      = var.vpc_cidr
-#  vpc_id        = module.vpc.vpc_id
-#  subnets = module.vpc.private_subnets
-#  bastion_node_cidr = var.bastion_node_cidr
-#
-#  desired_capacity = var.desired_capacity
-#  max_size         = var.max_size
-#  min_size         = var.min_size
-#}
+module "frontend" {
+  source = "./modules/app"
+  app_port      = 80
+  component     = "frontend"
+  env           = var.env
+  instance_type = "t3.micro"
+  vpc_cidr      = var.vpc_cidr
+  vpc_id        = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
+  bastion_node_cidr = var.bastion_node_cidr
+
+  desired_capacity = var.desired_capacity
+  max_size         = var.max_size
+  min_size         = var.min_size
+}
 #
 ##module for backend server
 module "backend" {
