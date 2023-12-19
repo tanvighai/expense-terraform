0bc1741f9fa59b5ce8ec2e77ed100ca9179e1e56
tanvighai <tanvighai02@gmail.com>
2023-12-18 21:13:14 -0500

    test commit

--- main.tf
+++ main.tf
@@ -14,59 +14,59 @@ module "vpc" {
 }
 ##here the vpc module is being used in the load balancer module so we need
 ##to store the output of above module in a file or variable so that we can call that here in this module
-module "public-lb" {
-  source = "./modules/alb"
-  alb_sg_allow_cidr = "0.0.0.0/0"
-  alb_type = "public"
-  env = var.env
-  internal = false
-  subnets = module.vpc.public_subnets
-  vpc_id = module.vpc.vpc_id
-}
-##module for private load balancer
-module "private-lb" {
-  source = "./modules/alb"
-  alb_sg_allow_cidr = "0.0.0.0/0"
-  alb_type = "private"
-  env = var.env
-  internal = true
-  subnets = module.vpc.private_subnets
-  vpc_id = module.vpc.vpc_id
-}
-
-#module for instances
-#module for frontend server
-module "frontend" {
-  source = "./modules/app"
-  app_port      = 80
-  component     = "frontend"
-  env           = var.env
-  instance_type = "t3.micro"
-  vpc_cidr      = var.vpc_cidr
-  vpc_id        = module.vpc.vpc_id
-  subnets = module.vpc.private_subnets
-  bastion_node_cidr = var.bastion_node_cidr
-
-  desired_capacity = var.desired_capacity
-  max_size         = var.max_size
-  min_size         = var.min_size
-}
-
-#module for backend server
-module "backend" {
-  source = "./modules/app"
-  app_port      = 8080
-  component     = "backend"
-  env           = var.env
-  instance_type = "t3.micro"
-  vpc_cidr      = var.vpc_cidr
-  vpc_id        = module.vpc.vpc_id
-  subnets = module.vpc.private_subnets
-  bastion_node_cidr = var.bastion_node_cidr
-  desired_capacity = var.desired_capacity
-  max_size         = var.max_size
-  min_size         = var.min_size
-}
+#module "public-lb" {
+#  source = "./modules/alb"
+#  alb_sg_allow_cidr = "0.0.0.0/0"
+#  alb_type = "public"
+#  env = var.env
+#  internal = false
+#  subnets = module.vpc.public_subnets
+#  vpc_id = module.vpc.vpc_id
+#}
+###module for private load balancer
+#module "private-lb" {
+#  source = "./modules/alb"
+#  alb_sg_allow_cidr = "0.0.0.0/0"
+#  alb_type = "private"
+#  env = var.env
+#  internal = true
+#  subnets = module.vpc.private_subnets
+#  vpc_id = module.vpc.vpc_id
+#}
+#
+##module for instances
+##module for frontend server
+#module "frontend" {
+#  source = "./modules/app"
+#  app_port      = 80
+#  component     = "frontend"
+#  env           = var.env
+#  instance_type = "t3.micro"
+#  vpc_cidr      = var.vpc_cidr
+#  vpc_id        = module.vpc.vpc_id
+#  subnets = module.vpc.private_subnets
+#  bastion_node_cidr = var.bastion_node_cidr
+#
+#  desired_capacity = var.desired_capacity
+#  max_size         = var.max_size
+#  min_size         = var.min_size
+#}
+#
+##module for backend server
+#module "backend" {
+#  source = "./modules/app"
+#  app_port      = 8080
+#  component     = "backend"
+#  env           = var.env
+#  instance_type = "t3.micro"
+#  vpc_cidr      = var.vpc_cidr
+#  vpc_id        = module.vpc.vpc_id
+#  subnets = module.vpc.private_subnets
+#  bastion_node_cidr = var.bastion_node_cidr
+#  desired_capacity = var.desired_capacity
+#  max_size         = var.max_size
+#  min_size         = var.min_size
+#}
 
 ##module for mysql
 
